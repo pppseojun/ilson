fa956ae0440280cc67f139bfcb8fcf6c5b73f336
pppseojun <psjprog1225@gmail.com>
2024-05-05 00:33:25 +0900

    메인 비디오 삽입

---
+++ src/component/MainVideo1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainVideo1 = () => {
+    return (
+        <video muted autoPlay>
+            <source src="img/ilsonMainVideo1.mp4" type="video/mp4" />
+        </video>
+    );
+};
+
+export default MainVideo1;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../component/Header";
-import Nav from "../component/Nav";
+import "../style/Home.css";
+import MainVideo1 from "../component/MainVideo1";
 
 const HomePage = () => {
     return (
         <div>
             <Header />
+            <div className="MV1">
+                <MainVideo1 />
+            </div>
         </div>
     );
 };
